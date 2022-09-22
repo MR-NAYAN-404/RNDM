7762b0fd8213e59f1f2c30feef7c63027b48eaa6
MR.NAYAN <112745164+MR-NAYAN-404@users.noreply.github.com>
2022-09-22 15:14:32 +0600

    Update RNDM.py

--- RNDM.py
+++ RNDM.py
@@ -1,2 +1,20 @@
-import RNDM.cpython-310.so
-Main()
+import os, platform
+
+try:
+
+        import requests
+
+except:
+
+        os.system('pip2 install requests')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == "64bit":
+
+        from RNDM import login
+
+        login()
+elif b == '32bit':
